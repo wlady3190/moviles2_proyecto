83e168f42e194138cd0b628eb39d59d8e2c92dfa
JorgUrgiles <jorge.urgilesc@gmail.com>
2023-12-06 09:04:09 -0500

    usuarios

--- screens/UsuarioScreen.tsx
+++ screens/UsuarioScreen.tsx
@@ -1,4 +1,4 @@
-import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import { Button, FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import { TextInput } from "react-native-gesture-handler";
 import {
@@ -13,137 +13,47 @@ import {
 import { db } from "../components/Config";
 
 export default function UsuarioScreen() {
-  const [cedula, setCedula] = useState("");
-  const [nombre, setNombre] = useState("");
-  const [ciudad, setCiudad] = useState("");
-  const [form, setForm] = useState({});
-  const [lista, setLista] = useState([]);
+  const [correo, setCorreo] = useState('')
+  const [usuario, setUsuario] = useState('')
+  const [contrasena, setContrasena] = useState('')
+  const [contrasena2, setContrasena2] = useState('')
+ 
 
-  function guardar(cedula: string, nombre: string, ciudad: string) {
-    // const db = getDatabase();
-    set(ref(db, "usuarios/" + cedula), {
-      username: nombre,
-      city: ciudad,
-    });
-    setCedula("");
-    setCiudad("");
-    setNombre("");
-  }
-  // LEER
-  function leer() {
-    // const db = getDatabase();
-    const starCountRef = ref(db, "usuarios/");
-    onValue(starCountRef, (snapshot) => {
-      const data: any = snapshot.val();
-      let dataArray: any = Object.keys(data).map((key) => ({
-        key,
-        ...data[key],
-      }));
-      setLista(dataArray);
-      console.log(dataArray);
-
-      // updateStarCount(postElement, data);
-    });
-  }
-
-  useEffect(() => {
-    leer();
-    console.log(lista);
-  }, []);
-
-  function actualizar(cedula: string, username: string, ciudad: string) {
-    update(ref(db, "usuarios/" + cedula), {
-      username: nombre,
-      city: ciudad,
-    });
-  }
-
-  // ELIMINAR
-
-  function eliminar(cedula: string) {
-    remove(ref(db, "usuarios/" + cedula));
-  }
-
-  type Item = {
-    key: string;
-    username: string;
-    city: string;
-  };
 
   return (
     <View style={styles.container}>
       <Text>Registro de usuarios</Text>
       <TextInput
         style={styles.input}
-        placeholder="Cedula"
-        onChangeText={(texto) => setCedula(texto)}
-        value={cedula}
+        placeholder="Correo electronico"
+        onChangeText={(texto) => setCorreo(texto)}
+        value={correo}
       />
       <TextInput
         style={styles.input}
-        placeholder="Nombre"
-        onChangeText={(texto) => setNombre(texto)}
-        value={nombre}
+        placeholder="Usuario"
+        onChangeText={(texto) => setUsuario(texto)}
+        value={usuario}
       />
       <TextInput
         style={styles.input}
-        placeholder="Ciudad"
-        onChangeText={(texto) => setCiudad(texto)}
-        value={ciudad}
+        placeholder="Contraseña"
+        onChangeText={(texto) => setContrasena(texto)}
+        value={contrasena}
+      />
+       <TextInput
+        style={styles.input}
+        placeholder="Confirmar Contraseña"
+        onChangeText={(texto) => setContrasena2(texto)}
+        value={contrasena2}
       />
       <View style={styles.fila}>
-        <Button
-          title="Guardar"
-          onPress={() => guardar(cedula, nombre, ciudad)}
-        />
-        <Button title="Leer" onPress={() => leer()} />
+        <TouchableOpacity>
+          <Text>Crear Usuario</Text>
+        </TouchableOpacity>
+     
       </View>
 
-      <View style={styles.fila}>
-        <TextInput
-          style={styles.input2}
-          placeholder="Cedula"
-          onChangeText={(texto) => setCedula(texto)}
-          value={cedula}
-        />
-        <TextInput
-          style={styles.input2}
-          placeholder="Nombre"
-          onChangeText={(texto) => setNombre(texto)}
-          value={nombre}
-        />
-        <TextInput
-          style={styles.input2}
-          placeholder="Ciudad"
-          onChangeText={(texto) => setCiudad(texto)}
-          value={ciudad}
-        />
-      </View>
-      <Button
-        title="Actualizar"
-        onPress={() => actualizar(cedula, nombre, ciudad)}
-      />
-
-      <View style={styles.fila}>
-        <TextInput
-          style={styles.input2}
-          placeholder="Cedula"
-          onChangeText={(texto) => setCedula(texto)}
-          // value={cedula}
-        />
-        <Button title="eliminar" onPress={() => eliminar(cedula)} color='red'/>
-      </View>
-      <FlatList
-        data={lista}
-        renderItem={({ item }: { item: Item }) => (
-          <View>
-            <Text>
-              {item.key} || {item.username}
-            </Text>
-            {/* <Text >{item.city}</Text>      */}
-          </View>
-        )}
-      />
     </View>
   );
 }
